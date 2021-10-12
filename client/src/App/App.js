4ece153c44241ac4a054d84835d2c5013b67dfbe
MJ <m3hm3tcan@gmail.com>
2021-10-12 21:56:50 +0300

    basic folder and prettier setup

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
 
 const App = () => {
   const [data, setData] = React.useState(null);
@@ -15,7 +13,6 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
