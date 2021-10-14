80b9a714843edd623d6c950368e1cc0ea306624e
MJ <m3hm3tcan@gmail.com>
2021-10-14 12:54:51 +0300

    get all couutry and search by text

--- client/src/Components/AllCountries/index.js
+++ client/src/Components/AllCountries/index.js
@@ -2,8 +2,45 @@ import React, { useEffect, useState } from "react";
 import "./AllCountries.css";
 
 const AllCountries = ({ countryList }) => {
-    return(
-        <div/>
+    const [searchText, setSearchText] = useState('');
+    const [filteredList, setFilteredList] = useState(countryList);
+    const [isOpen, setIsOpen]= useState(false);
+    const handleChange = (event) => {
+        
+        setSearchText(event.target.value)
+        let result = countryList.filter(item=> item.name.toLowerCase().includes(event.target.value.toLowerCase()));
+        setFilteredList(result);
+        event.target.value !== '' ? setIsOpen(true) : setIsOpen(false) 
+    }
+
+    return (
+     
+        <div className="container">
+            <div className="jumbotron">
+                <h3>All Countries</h3>
+                <p>Search from list of countries.</p>
+            </div>
+
+            <div className="row">
+                <div class="col-sm-3">
+                    <input type="text" className="form-control" id="coutry" value={searchText} onChange={handleChange} placeholder="Filter here" />
+                </div>
+                <div className="col-sm-3">
+                <div className={`dropdown ${isOpen ? "show" : ""}`}>
+                        <a className="btn btn-secondary dropdown-toggle" onClick={()=>setIsOpen(!isOpen)} href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Country List
+                        </a>
+
+                        <div className={`dropdown-menu ${isOpen ? "show" : ""}`} aria-labelledby="dropdownMenuLink">
+                            {filteredList.map(country=>(
+                                <a className="dropdown-item" href="#">{country.name}</a>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     )
 }
 
