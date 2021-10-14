284f0cd16e4d6bc8f7c177c52ae702965459b030
MJ <m3hm3tcan@gmail.com>
2021-10-14 23:02:24 +0300

    password encryption added

--- server/routes/userApi.js
+++ server/routes/userApi.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const axios = require("axios");
 const User = require("../models/user.model")
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
 router.post('/register', async (req, res) => {
     try {
+        const newPass = await bcrypt.hash(req.body.password,10)
         await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: newPass,
         })
         res.json({ message: "User registered!" });
     } catch (err) {
@@ -19,8 +21,12 @@ router.post('/register', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     try {
-        const user = await User.findOne({ email: req.body.email, password: req.body.password })
-        if (user) {
+        const user = await User.findOne({ email: req.body.email})
+        if(!user) {return res.json({ message: "User is invalid!", user: null})}
+
+        const isValidPassword = await bcrypt.compare(req.body.password, user.password)
+
+        if (user && isValidPassword) {
             const token = jwt.sign({
                 email: user.email,
                 name: user.name
